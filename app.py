b804ed934007cca6fdc68c36c33843116953cdf1
RootCloudAz <rootcloudaz@gmail.com>
2025-04-08 07:01:36 +0000

    changed app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, CI/CD! Welcome to our App"
+    return "Hello, CI/CD! Welcome to our App, We have successfully deployed our app on render!!"
 
 if __name__ == '__main__':
     app.run(debug=True)
